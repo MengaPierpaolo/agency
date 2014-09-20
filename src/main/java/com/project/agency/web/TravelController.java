73139f2bc9f3ebe795e09507c0deeca17bc0c348
Mario Rojas <mrojas.leyva@gmail.com>
2014-09-20 03:53:04 -0500

    Implementación de métodos para desplegar y recibir formulario de viaje nuevo

--- src/main/java/com/project/agency/web/TravelController.java
+++ src/main/java/com/project/agency/web/TravelController.java
@@ -1,24 +1,48 @@
 package com.project.agency.web;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.project.agency.domain.Travel;
+import com.project.agency.service.CityService;
 import com.project.agency.service.TravelService;
 
 @Controller
-public class HomeController {
+public class TravelController {
 	
 	@Autowired
 	private TravelService travelService;
+	
+	@Autowired
+	private CityService cityService;
 	
 	@RequestMapping("/")
 	public ModelAndView hello() {
 		ModelAndView modelAndView = new ModelAndView("hello");
 		modelAndView.addObject("travels", travelService.findAll());
-		
 		return modelAndView;
 	}
-
+	
+	@RequestMapping(value = "/travel/new", method = RequestMethod.GET)
+	public ModelAndView addTravel() {
+		ModelAndView modelAndView = new ModelAndView("travel/new");
+		modelAndView.addObject("newTravel", new Travel());
+		modelAndView.addObject("destinyList", cityService.findAll());		
+		return modelAndView;		
+	}
+	
+	@RequestMapping(value = "/travel/new", method = RequestMethod.POST)
+	public ModelAndView addTravelProcess(
+			@ModelAttribute("newTravel") @Valid Travel travel, BindingResult result) {
+		System.out.println(travel.toString());
+		System.out.println(travel.getCity().getCountryName());
+		return null;
+	}
 }
---
+++ src/main/webapp/WEB-INF/views/travel/new.jsp
@@ -0,0 +1,42 @@
+<%@include file="/WEB-INF/views/include.jsp" %>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+  <h1>New travel form</h1>
+  
+  <form:form action="" method="POST" modelAttribute="newTravel">
+    <p>
+      <form:label path="name">Travel Name</form:label>
+      <form:input path="name" />
+    </p>
+    <p>
+      <form:label path="city">Destiny</form:label>
+      <form:select path="city" items="${destinyList}" itemValue="id"  />
+    </p>
+    <p>
+      <form:label path="description">Description</form:label>
+      <form:textarea path="description" />
+    </p>
+    <p>
+      <form:label path="startDate">Start date</form:label>
+      <form:input path="startDate" />
+    </p>
+    <p>
+      <form:label path="endDate">End date</form:label>
+      <form:input path="endDate" />
+    </p>
+    <p>
+      <form:label path="price">Price</form:label>
+      <form:input path="price" />
+    </p>
+    <p>
+      <button type="submit" name="submitNewTravel">Save Travel</button>
+    </p>
+  </form:form>
+</body>
+</html>
