092062b2c641ddee38bf04292d9ea903b8a6407d
Mario Rojas <mrojas.leyva@gmail.com>
2014-09-20 03:54:28 -0500

    Generación de formateador para convertir de objeto ciudad a cadena de texto y viceversa

---
+++ src/main/java/com/project/agency/formatter/CityFormatter.java
@@ -0,0 +1,34 @@
+package com.project.agency.formatter;
+
+import java.text.ParseException;
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.Formatter;
+import org.springframework.stereotype.Component;
+
+import com.project.agency.domain.City;
+import com.project.agency.service.CityService;
+
+/**
+ * Utilizado para desplegar en un form:select al objeto como una cadena
+ * de texto y que al ser enviado al controlador pase como objeto nuevamente
+ * en lugar de un valor nulo
+ */
+@Component
+public class CityFormatter implements Formatter<City> {
+	
+	@Autowired
+	private CityService cityService;
+
+	@Override
+	public String print(City city, Locale locale) {
+		return city.getDestinyName();
+	}
+
+	@Override
+	public City parse(String cityId, Locale locale) throws ParseException {
+		return cityService.getCity(Long.parseLong(cityId, 10));
+	}
+
+}
